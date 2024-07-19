66655f55d2e535e777e9c396210f936d30881422
spandananadendla <spandananadendla2000@gmail.com>
2024-07-19 11:13:31 +0530

    day2

---
+++ day2/multiplersOfThree.py
@@ -0,0 +1,8 @@
+a=int(input("Enter any number"))
+b=a%10
+
+if (b%3==0):
+    print("The number is divisble by 3")
+else:
+    print("The number is not divisible by 3")
+    
--- day2/multipliersOfTen.py
+++ day2/multipliersOfTen.py
@@ -1,1 +1,6 @@
 a=int(input("Enter any number"))
+if (a%10==0):
+    print("Hiii!")
+else:
+    print("Bye!")
+    
